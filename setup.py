9af33264883d1b2691542f8aa4ac4dc01cf8763a
Jozsa, Bela - Tamas <jozsa.bela.tamas@gmail.com>
2017-12-25 17:17:43 +0200

    Server Project Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tcdataimporter',
+      version='0.1',
+      description='Teamcenter Data Importer',
+      url='http://github.com/storborg/funniest',
+      author='Bela - Tamas Jozsa',
+      author_email='jozsa.bela.tamas@gmail.com',
+      license='Apache 2.0',
+      packages=['tcdataimporter'],
+      zip_safe=False)
